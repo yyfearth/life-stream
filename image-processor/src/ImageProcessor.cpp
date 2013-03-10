19997eb11909efbf120f7892820638c5e12063aa
Wilson Young <yyfearth@gmail.com>
2013-03-10 15:58:35 -0700

    Create socket connection with protobuf but failed to parse and send

--- image-processor/src/ImageProcessor.cpp
+++ image-processor/src/ImageProcessor.cpp
@@ -7,19 +7,22 @@
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
 
+#include "meta.pb.h"
+
 // #include "exif.h"
 
 using namespace std;
 
 using boost::asio::ip::tcp;
 
-const size_t BUF_LEN = 128;
+const size_t BUF_LEN = 1024;
 
-class TCPClient
+class Client
 {
 public:
-    TCPClient(boost::asio::io_service& io_service, tcp::resolver::iterator endpoint_iterator);
+    Client(boost::asio::io_service& io_service, tcp::resolver::iterator endpoint_iterator);
 
+    void write();
     void close();
 private:
     boost::asio::io_service& io_service;
@@ -33,60 +36,114 @@ protected:
 
     void on_receive(const boost::system::error_code& err_code);
 
+    void on_write(const boost::system::error_code& error);
+
     void do_close();
 };
 
-TCPClient::TCPClient(boost::asio::io_service& io_svc, tcp::resolver::iterator endpoint_iterator)
+Client::Client(boost::asio::io_service& io_svc, tcp::resolver::iterator endpoint_iterator)
 : io_service(io_svc), socket(io_svc)
 {
 
     tcp::endpoint endpoint = *endpoint_iterator;
 
     socket.async_connect(endpoint,
-            boost::bind(&TCPClient::on_connect, this, boost::asio::placeholders::error, ++endpoint_iterator));
+            boost::bind(&Client::on_connect, this, boost::asio::placeholders::error, ++endpoint_iterator));
 }
 
-void TCPClient::close()
+void Client::close()
 {
-    io_service.post(boost::bind(&TCPClient::do_close, this));
+    std::cout << "close requested" << std::endl;
+    io_service.post(boost::bind(&Client::do_close, this));
 }
 
-void TCPClient::on_connect(const boost::system::error_code& err_code,
+void Client::on_connect(const boost::system::error_code& err_code,
     tcp::resolver::iterator endpoint_iterator)
 {
     if (err_code == 0)
     // Successful connected
     {
+        std::cout << "connected" << std::endl;
+
+        write();
+
         socket.async_receive(boost::asio::buffer(buffer.data(), BUF_LEN),
-                boost::bind(&TCPClient::on_receive, this, boost::asio::placeholders::error));
+                boost::bind(&Client::on_receive, this, boost::asio::placeholders::error));
 
 
     } else if (endpoint_iterator != tcp::resolver::iterator())
     {
+        std::cout << "connect failed" << std::endl;
         socket.close();
         tcp::endpoint endpoint = *endpoint_iterator;
 
         socket.async_connect(endpoint,
-                boost::bind(&TCPClient::on_connect, this, boost::asio::placeholders::error, ++endpoint_iterator));
+                boost::bind(&Client::on_connect, this, boost::asio::placeholders::error, ++endpoint_iterator));
     }
 }
 
-void TCPClient::on_receive(const boost::system::error_code& err_code)
+void Client::on_receive(const boost::system::error_code& err_code)
 {
     if (err_code == 0)
     {
-        std::cout << buffer.data() << std::endl;
+        string str(buffer.data());
+        std::cout << "received: " << str.length() << std::endl;
+
+        meta::Image image;
+        if (image.ParseFromString(str)) {
+            std::cout << image.DebugString() << std::endl;
+        } else {
+            std::cout << "failed to parse" << std::endl;
+        }
 
         socket.async_receive(boost::asio::buffer(buffer.data(), BUF_LEN),
-                boost::bind(&TCPClient::on_receive, this, boost::asio::placeholders::error));
+                boost::bind(&Client::on_receive, this, boost::asio::placeholders::error));
     } else {
         do_close();
     }
 }
 
-void TCPClient::do_close()
+void Client::write()
 {
+    meta::Image image;
+    image.set_uuid("xxxxxxxxxxxxxxxxxxxxx");
+    image.set_filename("test1.jpg");
+    image.set_processed(true);
+    string buf;
+    image.SerializeToString(&buf);
+    std::cout << "write " << buf.length() << std::endl << image.DebugString() << std::endl;
+    boost::asio::write(socket, boost::asio::buffer(buf.c_str(), buf.length()));
+    // boost::asio::async_write(socket,
+    //     boost::asio::buffer(buf, buf.length()),
+    //     boost::bind(&Client::on_write, this, boost::asio::placeholders::error));
+
+}
+
+void Client::on_write(const boost::system::error_code& error)
+{
+    if (!error)
+    {
+        // meta::Image image;
+        // image.set_uuid("xxxxxxxxxxxxxxxxxxxxx");
+        // image.set_filename("test1.jpg");
+        // image.set_processed(true);
+        // string buf;
+        // image.SerializeToString(&buf);
+        // boost::asio::async_write(socket,
+        //     boost::asio::buffer(buf, buf.length()),
+        //     boost::bind(&Client::on_write, this, boost::asio::placeholders::error));
+    }
+    else
+    {
+        do_close();
+    }
+}
+
+void Client::do_close()
+{
+    std::cout << "do close" << std::endl;
     socket.close();
+
 }
 
 int main(int argc, char* argv[])
@@ -105,17 +162,17 @@ int main(int argc, char* argv[])
 
         tcp::resolver::iterator endpoint_iteratorator = resolver.resolve(query);
 
-        TCPClient client(io_service, endpoint_iteratorator);
+        Client client(io_service, endpoint_iteratorator);
 
         boost::thread thread(
                 boost::bind(&boost::asio::io_service::run, &io_service));
 
         std::cout << "Client started." << std::endl;
 
-        std::string Input;
-        while (Input != "exit")
+        std::string input;
+        while (input != "exit")
         {
-            std::cin >> Input;
+            std::cin >> input;
         }
 
         client.close();
