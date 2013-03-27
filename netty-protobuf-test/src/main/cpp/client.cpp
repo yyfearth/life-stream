5c2f9a0a94b245fc0f466a0bf42fd9014c788d71
Wilson Young <yyfearth@gmail.com>
2013-03-26 17:33:26 -0700

    C++ client test successful (w/r)

--- netty-protobuf-test/src/main/cpp/client.cpp
+++ netty-protobuf-test/src/main/cpp/client.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include <boost/array.hpp>
+#include <boost/scoped_array.hpp>
 #include <boost/asio.hpp>
 
 #include "meta.pb.h"
@@ -12,6 +12,11 @@ using namespace std;
 using boost::asio::ip::tcp;
 
 const size_t BUF_LEN = 1024;
+
+void flip(google::protobuf::uint32 &dword)
+{
+    dword = ((dword>>24)&0x000000FF) | ((dword>>8)&0x0000FF00) | ((dword<<8)&0x00FF0000) | ((dword<<24)&0xFF000000);
+}
 
 int main(int argc, char* argv[])
 {
@@ -35,22 +40,35 @@ int main(int argc, char* argv[])
         std::cout << "client connected" << std::endl;
 
         if (1) { // read
-            boost::array<char, 1024> buf;
+            google::protobuf::uint32 size(0);
             boost::system::error_code error;
+            size_t len;
+            len = boost::asio::read(socket, boost::asio::buffer(&size, sizeof(size)), error);
 
-            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+            if (error == boost::asio::error::eof)
+                size = 0; // Connection closed cleanly by peer.
+            else if (error)
+                throw boost::system::system_error(error); // Some other error.
+            else
+                flip(size);
 
+            std::cout << len << size << std::endl;
+
+            boost::scoped_array<char> buf(new char[size]);
+
+            len = socket.read_some(boost::asio::buffer(buf.get(), size), error);
             if (error == boost::asio::error::eof)
                 len = 0; // Connection closed cleanly by peer.
             else if (error)
                 throw boost::system::system_error(error); // Some other error.
 
+
             if (len) {
                 std::cout << "get binary data: " << len << std::endl;
-                std::cout.write(buf.data(), len);
+                std::cout.write(buf.get(), len);
                 std::cout << std::endl;
 
-                string str(buf.data(), len);
+                string str(buf.get(), len);
                 meta::Image image;
                 if (image.ParseFromString(str)) {
                     std::cout << image.DebugString() << std::endl;
@@ -61,17 +79,18 @@ int main(int argc, char* argv[])
                 std::cout << "no content recieved" << std::endl;
         }
 
-        if (0) { // write
+        if (1) { // write
             meta::Image image;
-            image.set_uuid("xxxxxxxxxxxxxxxxxxxxx");
+            image.set_uuid("a92b3363-9530-44e8-95ef-588facc74969");
             image.set_filename("test1.jpg");
             image.set_processed(true);
             string buf;
             image.SerializeToString(&buf);
             std::cout << "write " << buf.length() << std::endl << image.DebugString() << std::endl;
             google::protobuf::uint32 size(buf.length());
-            boost::asio::write(socket, boost::asio::buffer(&size, sizeof(size)));
-            boost::asio::write(socket, boost::asio::buffer(buf, buf.length()));
+            flip(size);
+            socket.write_some(boost::asio::buffer(&size, sizeof(size)));
+            socket.write_some(boost::asio::buffer(buf.c_str(), buf.length()));
             std::cout << "sent" << std::endl;
         }
 
